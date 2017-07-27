9b49d1d4e7ef190eb03797782446f05d848aa07e
maxxie114 <maxxie114@gmail.com>
2017-07-27 13:27:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,76 +7,103 @@ pieces = gameboard.pieces
 player_letter = None
 player2_letter = None
 turn = None
+sizeW = 560
+sizeH = 560
+blackTurn = False
+
+whitechess = ["♙", "♘" , "♗" , "♖" , "♕" , "♔" ]
 
 
+blackChess = ["♟" ,  "♞" ,"♝" ,"♜", "♛" , "♚"]#add all the black pieces as string.
 
 
-#start the game
-def get_starting_player():
-  random_num = 1  #This will lock the entire game to white only, and it must be changed
-  # print("get_starting_player() functional")  #test code
-  if random_num == 0:
-      # print("random = 0")  #test code
-      return "player"
-  else:
-      # print("random = 1") #test code
-      return "player2"
+# #start the game
+# def get_starting_player():
+#   random_num = 1 
+#   # print("get_starting_player() functional")  #test code
+#   if random_num == 0:
+#       # print("random = 0")  #test code
+#       return "player"  #black
+#   else:
+#       # print("random = 1") #test code
+#       return "player2"  #white
 
 
 #run player turn	
-def run_player_turn():
+def run_player_turn():  #
   global board
   global pieces
-  # print("run_player_turn() functional")   #test code
   if mouse.pressed and board[mouse.y//70][mouse.x//70][0] != None:
     pieces.insert(0, board[mouse.y//70][mouse.x//70][0])
-    # print(pieces) #test code
+    print(pieces)
     board[mouse.y//70][mouse.x//70][0] = None
-    # gameboard.drawBoard(board)
-    pieces.pop()
+    
+    gameboard.drawBoard(board)
+    pieces.pop(1)
+    print(pieces)
+    gameboard.drawBoard(board)
     return True
-    # print(pieces) #test code
   elif  mouse.pressed and board[mouse.y//70][mouse.x//70][0] == None:
     # gameboard.drawBoard(board)
-    # board[mouse.y//70][mouse.x//70][0] = pieces[number]
+    gameboard.drawBoard(board)
+    print(pieces)
+    board[mouse.y//70][mouse.x//70][0] = pieces[0]
+    gameboard.drawBoard(board)
     return True
   else:
     return False #erase the og pieces
     
-    
 def run_Player2_turn():
   global board
   global pieces
-  number = 0
+  # number = 0
   # print("2")
   if mouse.pressed and board[mouse.y//70][mouse.x//70][0] != None:
     pieces.insert(0, board[mouse.y//70][mouse.x//70][0])
-    print(pieces) #test code
+    print(pieces)
     board[mouse.y//70][mouse.x//70][0] = None
+    # gameboard.drawBoard(board)
+    pieces.pop(1)
+    print(pieces)
     gameboard.drawBoard(board)
-    pieces.pop()
-    # print(pieces) #test code
     return True
   elif  mouse.pressed and board[mouse.y//70][mouse.x//70][0] == None:
+    # gameboard.redrawSequence(board)
+    print(pieces)
+    board[mouse.y//70][mouse.x//70][0] = pieces[0]
     gameboard.drawBoard(board)
-    # board[mouse.y//70][mouse.x//70][0] = pieces[number]
     return True
   else:
     return False
- 
-              
-              
-
-#Determine lost or win
     
+def isBlackTurn(chess): #should be a string
+   global whitechess
+   global blackChess
+   for w in range(len(whitechess)):
+     if chess == whiteChess[w]:
+       return False
+   for b in range(len(blackChess)):
+     if chess == blackChess[b]:
+       return True
+   return False
+   
+def isBlackTurn(chess): #should be a string
+   global whitechess
+   global blackChess
+   for w in range(len(whitechess)):
+     if chess == whiteChess[w]:
+       return True
+   for b in range(len(blackChess)):
+     if chess == blackChess[b]:
+       return False
+   return True
 #settings (setup() only run once)
 def setup():
-	size(560,560)
+	size(sizeW, sizeH)
 	global board
 	global turn
 	global player_letter
 	global player2_letter
-# 	print("setup() functional")
 	#draw the board
 	gameboard.drawBoard(board)
 	gameboard.drawLetters(board)
@@ -84,13 +111,7 @@ def setup():
 	#drawLetters(board)
 	
   #determine the text to print
-	turn = get_starting_player()
-	if turn == "player":
-	  player_letter = "♙"
-	  player2_letter = "♟"
-	else:
-	  player_letter = "♟"
-	  player2_letter = "♙"
+	turn = "player2"
 
 	  
 #Main function (Will loops forever)
@@ -100,21 +121,19 @@ def draw():
   global player_letter
   global player2_letter
   global pieces
-  # print("draw() functional")  #test code
-  # print("turn =", turn) #test code
   #run all the functions that is needed
   # drawBoard(board)
   # print(pieces)
   if turn == "player":
-    if run_player_turn():
-      # print("ran player turn") #test code
-      print(board)
+    if run_player_turn():  
+      # gameboard.redrawSequence(board)
+      gameboard.redrawSequence(board)
       turn = "player2"
   elif turn == "player2":
     if run_Player2_turn():
-      # print("ran player2 turn")  #test code
+      gameboard.redrawSequence(board)
       turn = "player"
-      gameboard.movePieces(board)
+      
 
     
   
